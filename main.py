b0bafdb020a7cf2139a0ded49235428bd4943f82
rlafbfdk <ruula751482@naver.com>
2021-11-16 15:32:00 +0900

    네이버 영화 제목 수집(+모듈화)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 # 만든이: 김률아
 # 일자: 2021.11.09
 
+import math
 import requests
 from bs4 import BeautifulSoup
 import webcrawl.WebCrawlService as wcs
@@ -23,6 +24,15 @@ movie_code = '209496'  # 네이버 영화 code
 # 1. 제목 수집
 title = wcs.get_movie_title(movie_code)
 print(title)
+
+# 2. 전체 페이지수 계산
+pages = wcs.calc_pages(movie_code)
+print(pages)
+
+# 3.리뷰 수집
+wcs.get_reviews(movie_code, pages, title)
+
+
 
 
 
